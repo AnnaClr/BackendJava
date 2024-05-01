90ad55ec2e745732991ebfbaddb6c2df6f5ace0f
Ana Clara <annaclarag2005@gmail.com>
2024-05-01 19:50:33 -0300

    aula tipos e variaveis

--- tipos-e-variaveis/src/TipoDados.java
+++ tipos-e-variaveis/src/TipoDados.java
@@ -25,14 +25,14 @@ public class TipoDados {
 		 * esta linha é considerada como declaração de variável iniciamos a existência
 		 * variavel numero com valor 5 regra: tipo + nome + valor
 		*/
-		int numero = 5;
+		// int numero = 5;
 
 		/*
 		 * na linha abaixo iremos alterar o valor do varíavel para 10 observe que o tipo
 		 * não é mais necessário, pois a variável já foi declarada anteriormente
 		*/
-		numero = 10;
-		System.out.print(numero);
+		// numero = 10;
+		// System.out.print(numero);
 		
 		/*
 		 * ao usar a palavra reservada final, você determina que jamais
@@ -41,7 +41,7 @@ public class TipoDados {
 		 * isso é considerado uma CONSTANTE na linguagem Java
 		*/
 
-		final double VALOR_DE_PI = 3.14;
+		// final double VALOR_DE_PI = 3.14;
 		// VALOR_DE_PI=3.15; // Esta linha vai apresentar erro de compilação!
 
 	}
