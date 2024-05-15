c7f60b4c25fce3e5d5eb7249cd55e8b51a202a3e
Ana Clara <annaclarag2005@gmail.com>
2024-05-15 14:02:15 -0300

    DESAFIO: Contador

--- PROJETOS_BOOTCAMP/2.processo-seletivo/src/ProcessoSeletivo.java
+++ PROJETOS_BOOTCAMP/2.processo-seletivo/src/ProcessoSeletivo.java
@@ -1,6 +1,5 @@
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
-
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         seleçãoCandidatos();
